c1b7c997cf26525d60695bb08a940f45f910373d
xav <91441149+xav-temple@users.noreply.github.com>
2022-11-01 16:31:56 -0400

    All of xavs work from earlier branch

--- apps/dapp/src/providers/AppProvider.tsx
+++ apps/dapp/src/providers/AppProvider.tsx
@@ -12,6 +12,7 @@ import { VaultContextProvider } from 'components/Pages/Core/VaultContext';
 import { noop } from 'utils/helpers';
 import { ConnectorPopover } from 'components/Layouts/CoreLayout/ConnectorPopover';
 import { WrongNetworkPopover } from 'components/Layouts/CoreLayout/WrongNetworkPopover';
+import { RelicProvider } from './RelicProvider';
 
 interface AppProviderState {
   showConnectPopover: () => void;
@@ -38,16 +39,18 @@ export const AppProvider = (props: PropsWithChildren<{}>) => {
             <StakingProvider>
               <FaithProvider>
                 <VaultContextProvider>
-                  <ThemeProvider theme={theme}>
-                    <AppContext.Provider value={{ showConnectPopover }}>
-                      <ConnectorPopover
-                        isOpen={connectPopoverVisible}
-                        onClose={() => setConnectPopoverVisibile(false)}
-                      />
-                      <WrongNetworkPopover />
-                      {props.children}
-                    </AppContext.Provider>
-                  </ThemeProvider>
+                  <RelicProvider>
+                    <ThemeProvider theme={theme}>
+                      <AppContext.Provider value={{ showConnectPopover }}>
+                        <ConnectorPopover
+                          isOpen={connectPopoverVisible}
+                          onClose={() => setConnectPopoverVisibile(false)}
+                        />
+                        <WrongNetworkPopover />
+                        {props.children}
+                      </AppContext.Provider>
+                    </ThemeProvider>
+                  </RelicProvider>
                 </VaultContextProvider>
               </FaithProvider>
             </StakingProvider>
